c50660ba5669053a8f8bee1193a328dc2d7d3efd
mrloureed <mrloureed@gmail.com>
2025-04-02 22:12:54 -0400

    remove prime as no longer using

--- pour/web/tailwind.config.ts
+++ pour/web/tailwind.config.ts
@@ -1,10 +1,8 @@
 import type { Config } from "tailwindcss";
-import { theme } from '@viamrobotics/prime-core/theme';
 
 export default {
   content: [
     "./src/**/*.{html,js,svelte,ts}",
     "./index.html",
   ],
-  theme,
 } satisfies Config;
