c7d62dcda68e1db261b73d764290b759610369ca
Eliot Horowitz <eliothorowitz@gmail.com>
2025-06-04 22:41:58 -0400

    fix tests

--- pour/vinocart_test.go
+++ pour/vinocart_test.go
@@ -23,23 +23,23 @@ func TestGetApproachPoint(t *testing.T) {
 	c := r3.Vector{233.8, 202.05, 85.0}
 
 	p := getApproachPoint(md, c, 100, &spatialmath.OrientationVectorDegrees{OX: 1}, logger)
-	test.That(t, p.X, test.ShouldAlmostEqual, 100, 1)
+	test.That(t, p.X, test.ShouldAlmostEqual, c.X-100, 1)
 	test.That(t, p.Y, test.ShouldAlmostEqual, c.Y, 1)
 	test.That(t, p.Z, test.ShouldAlmostEqual, c.Z, 1)
 
 	p = getApproachPoint(md, c, 100, &spatialmath.OrientationVectorDegrees{OX: -1}, logger)
-	test.That(t, p.X, test.ShouldAlmostEqual, 367.7, 1)
+	test.That(t, p.X, test.ShouldAlmostEqual, c.X+100, 1)
 	test.That(t, p.Y, test.ShouldAlmostEqual, c.Y, 1)
 	test.That(t, p.Z, test.ShouldAlmostEqual, c.Z, 1)
 
 	p = getApproachPoint(md, c, 100, &spatialmath.OrientationVectorDegrees{OY: 1}, logger)
 	test.That(t, p.X, test.ShouldAlmostEqual, c.X, 1)
-	test.That(t, p.Y, test.ShouldAlmostEqual, md.MinY-100, 1)
+	test.That(t, p.Y, test.ShouldAlmostEqual, c.Y-100, 1)
 	test.That(t, p.Z, test.ShouldAlmostEqual, c.Z, 1)
 
 	p = getApproachPoint(md, c, 100, &spatialmath.OrientationVectorDegrees{OY: -1}, logger)
 	test.That(t, p.X, test.ShouldAlmostEqual, c.X, 1)
-	test.That(t, p.Y, test.ShouldAlmostEqual, md.MaxY+100, 1)
+	test.That(t, p.Y, test.ShouldAlmostEqual, c.Y+100, 1)
 	test.That(t, p.Z, test.ShouldAlmostEqual, c.Z, 1)
 
 }
