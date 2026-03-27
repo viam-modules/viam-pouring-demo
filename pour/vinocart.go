f5f2ddc61b6a35d2802e5cc396079d9dcbd6dc20
Michael Lee <michaellee1019@gmail.com>
2026-03-27 15:46:17 -0400

    universal bottle to cup offset

--- pour/vinocart.go
+++ pour/vinocart.go
@@ -97,7 +97,7 @@ func NewVinoCart(ctx context.Context, conf *Config, c *Pour1Components, client r
 	vc.cupTop = referenceframe.NewLinkInFrame(
 		vc.conf.GripperName,
 		spatialmath.NewPose(
-			r3.Vector{X: vc.conf.cupGripHeightOffset(), Y: -75, Z: -35},
+			r3.Vector{X: vc.conf.cupGripHeightOffset(), Y: -65, Z: -5},
 			&spatialmath.OrientationVectorDegrees{OX: 1},
 		),
 		cupTopName,
