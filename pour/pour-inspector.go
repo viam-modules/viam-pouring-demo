b183b9dade5601aa55e2ef4046c3b54b16a2e9f3
Naveed Jooma <naveed@viam.com>
2026-05-07 15:04:23 -0400

    More refactor

--- pour/pour-inspector.go
+++ pour/pour-inspector.go
@@ -5,10 +5,8 @@ import (
 	"errors"
 	"fmt"
 	"image"
-	"io/fs"
 	"os"
 	"path/filepath"
-	"sort"
 	"time"
 
 	"go.viam.com/rdk/app"
@@ -18,17 +16,23 @@ import (
 	"go.viam.com/rdk/utils"
 )
 
-type TrainingLabel string
-
-const goodPour = TrainingLabel("good-pour")
-const underPour = TrainingLabel("under-pour")
-const overPour = TrainingLabel("over-pour")
+// Dataset to upload/train pour quality on
 const pourQualityDatasetID = "69ebb260ec6ccb3ae5466448"
-const trainingDataDirName = "training"
 
-func dirnameForPour(pour time.Time) string {
-	return filepath.Join(trainingDataDirName, pour.Format("20060102_150405.000"))
-}
+// Training label indicating a good pour
+const goodPour = TrainingLabel("good-pour")
+
+// Training label indicating an under pour
+const underPour = TrainingLabel("under-pour")
+
+// Training label indicating an over pour
+const overPour = TrainingLabel("over-pour")
+
+// The point after which images are ignored
+const staleTime = 2 * time.Minute
+
+// The score of the classification to be considered "accurate"
+const classificationThreshold = 0.80
 
 // Inspects and trains pours.
 // Use to detect if a pour is considered "good" via CV,
@@ -37,81 +41,58 @@ func dirnameForPour(pour time.Time) string {
 type pourInsepctor struct {
 	visionService vision.Service
 	dataClient    *app.DataClient
-	cameraName    *resource.Name
+	cameraName    resource.Name
 	logger        logging.Logger
 }
 
 // Check if the pour is considered good using computer vision
-func (pi *pourInsepctor) checkGoodPour(ctx context.Context, img image.Image) bool {
+func (pi *pourInsepctor) checkGoodPour(ctx context.Context, img image.Image) (bool, error) {
 	if pi.visionService == nil {
-		return false
+		return false, errors.New("no vision service provided")
 	}
 	classifications, err := pi.visionService.Classifications(ctx, img, 1, nil)
 	if err != nil {
-		return false
+		return false, err
 	}
 	if len(classifications) == 0 {
-		return false
+		pi.logger.Debug("[PI] Classifications length is zero")
+		return false, nil
 	}
 
 	classification := classifications[0]
 	label := classification.Label()
 	score := classification.Score()
-	if label == string(goodPour) && score >= 80 {
-		return true
+	pi.logger.Debugf("[PI] Pour classification: %s - %0.2f", label, score)
+
+	// We want to stop on a good pour or over pour
+	if (label == string(goodPour) || label == string(overPour)) && score >= classificationThreshold {
+		return true, nil
 	}
-	return false
+
+	return false, nil
 }
 
 // Label the images from the provided pour with the given label.
 //
 //   - If the label is an `under-pour`, then the most last two images of the pour will be labaled as such.
 //   - If the label is a `good-pour`, then the last image will be labled a `good-pour`, and the two images prior will be labeled `under-pour`
-//   - If the label is an `over-pour`, then the last image will be labeled an `over-pour`,
+//   - If the label is an `over-pour`, then the last two images will be labeled an `over-pour`,
 //     the two images prior will be labeled `good-pour`,
 //     and the two images further prior will be labeled `under-pour`
 func (pi *pourInsepctor) labelPour(ctx context.Context, pour time.Time, label TrainingLabel) error {
 	folderName := dirnameForPour(pour)
 	defer func() {
-		if err := pi.cleanupImages(folderName); err != nil {
+		if err := cleanupImages(folderName); err != nil {
 			pi.logger.Errorf("failed to cleanup images: %v", err)
 		}
 	}()
 
-	if age := time.Since(pour); age > 2*time.Minute {
+	if age := time.Since(pour); age > staleTime {
 		pi.logger.Infof("skipping upload of stale images (age: %v): %s", age, folderName)
 		return nil
 	}
 
 	return pi.uploadTaggedImages(ctx, folderName, label)
-}
-
-func findFiles(ctx context.Context, root string) ([]string, error) {
-	var files []string
-	if err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-		if d.IsDir() {
-			return nil
-		}
-		files = append(files, path)
-		return nil
-	}); err != nil {
-		return nil, err
-	}
-	sort.Strings(files)
-	return files, nil
-}
-
-func (pi *pourInsepctor) cleanupImages(dir string) error {
-	if dir == "" {
-		return nil
-	}
-	if err := os.RemoveAll(dir); err != nil {
-		return err
-	}
-	return nil
 }
 
 func (pi *pourInsepctor) uploadTaggedImages(ctx context.Context, folderPath string, label TrainingLabel) error {
@@ -120,15 +101,20 @@ func (pi *pourInsepctor) uploadTaggedImages(ctx context.Context, folderPath stri
 		return fmt.Errorf("%s not defined", utils.MachinePartIDEnvVar)
 	}
 	pourTime := filepath.Base(folderPath)
-	notFullOpts := &app.FileUploadOptions{
+
+	underPourOpts := &app.FileUploadOptions{
 		ComponentName: &pi.cameraName.Name,
-		Tags:          []string{"not-full", pourTime},
+		Tags:          []string{string(underPour), pourTime},
 		DatasetIDs:    []string{pourQualityDatasetID},
 	}
-
-	fullOpts := &app.FileUploadOptions{
+	goodPourOpts := &app.FileUploadOptions{
 		ComponentName: &pi.cameraName.Name,
-		Tags:          []string{"full", pourTime},
+		Tags:          []string{string(goodPour), pourTime},
+		DatasetIDs:    []string{pourQualityDatasetID},
+	}
+	overPourOpts := &app.FileUploadOptions{
+		ComponentName: &pi.cameraName.Name,
+		Tags:          []string{string(overPour), pourTime},
 		DatasetIDs:    []string{pourQualityDatasetID},
 	}
 
@@ -153,7 +139,7 @@ func (pi *pourInsepctor) uploadTaggedImages(ctx context.Context, folderPath stri
 		endBoundary := max(0, numFiles-2)
 
 		for _, filepath := range files[endBoundary:] {
-			if _, err := pi.dataClient.FileUploadFromPath(ctx, pid, filepath, notFullOpts); err != nil {
+			if _, err := pi.dataClient.FileUploadFromPath(ctx, pid, filepath, underPourOpts); err != nil {
 				return err
 			}
 		}
@@ -165,32 +151,42 @@ func (pi *pourInsepctor) uploadTaggedImages(ctx context.Context, folderPath stri
 		oneFromEndBoundary := max(0, numFiles-1)
 
 		for _, path := range files[threeFromEndBoundary:oneFromEndBoundary] {
-			if _, err := pi.dataClient.FileUploadFromPath(ctx, pid, path, notFullOpts); err != nil {
+			if _, err := pi.dataClient.FileUploadFromPath(ctx, pid, path, underPourOpts); err != nil {
 				return err
 			}
 		}
 		// tag the last image as full
 		if numFiles > 0 {
-			if _, err := pi.dataClient.FileUploadFromPath(ctx, pid, files[numFiles-1], fullOpts); err != nil {
+			if _, err := pi.dataClient.FileUploadFromPath(ctx, pid, files[numFiles-1], goodPourOpts); err != nil {
 				return err
 			}
 		}
 
-	// We assume that the last 3 images are full, this is an approximation of the amount of over-pouring
-	// We also label the 2 images before the full/over-pour section as not-full
+	// We assume that the last 2 images are over-pour,
+	// the two before that as good-pour,
+	// and the two before that as under-pour.
+	//
+	// This is an approximation of the amount of over-pouring
 	case overPour:
 		// guard against index out of bounds
-		fiveFromEndBoundary := max(0, numFiles-5)
-		threeFromEndBoundary := max(0, numFiles-3)
+		sixFromEndBoundary := max(0, numFiles-6)
+		fourFromEndBoundary := max(0, numFiles-4)
+		twoFromEndBoundary := max(0, numFiles-2)
 
-		for _, path := range files[fiveFromEndBoundary:threeFromEndBoundary] {
-			if _, err := pi.dataClient.FileUploadFromPath(ctx, pid, path, notFullOpts); err != nil {
+		for _, path := range files[sixFromEndBoundary:fourFromEndBoundary] {
+			if _, err := pi.dataClient.FileUploadFromPath(ctx, pid, path, underPourOpts); err != nil {
 				return err
 			}
 		}
 
-		for _, path := range files[threeFromEndBoundary:] {
-			if _, err := pi.dataClient.FileUploadFromPath(ctx, pid, path, fullOpts); err != nil {
+		for _, path := range files[fourFromEndBoundary:twoFromEndBoundary] {
+			if _, err := pi.dataClient.FileUploadFromPath(ctx, pid, path, goodPourOpts); err != nil {
+				return err
+			}
+		}
+
+		for _, path := range files[twoFromEndBoundary:] {
+			if _, err := pi.dataClient.FileUploadFromPath(ctx, pid, path, overPourOpts); err != nil {
 				return err
 			}
 		}
---
+++ pour/training_utils.go
@@ -0,0 +1,46 @@
+package pour
+
+import (
+	"context"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"sort"
+	"time"
+)
+
+type TrainingLabel string
+
+const trainingDataDirName = "training"
+
+func dirnameForPour(pour time.Time) string {
+	return filepath.Join(trainingDataDirName, pour.Format("20060102_150405.000"))
+}
+
+func findFiles(ctx context.Context, root string) ([]string, error) {
+	var files []string
+	if err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			return nil
+		}
+		files = append(files, path)
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	sort.Strings(files)
+	return files, nil
+}
+
+func cleanupImages(dir string) error {
+	if dir == "" {
+		return nil
+	}
+	if err := os.RemoveAll(dir); err != nil {
+		return err
+	}
+	return nil
+}
